0fcd3513109fad6fba7af8135a80e413476dbcd8
HaierKeys <haierkeys@gmail.com>
2024-12-31 01:20:40 +0800

    代理代码

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,45 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+
+const proxys = {
+	'github.com': 'https://github.com',
+	'github.io': 'https://github.io',
+}
+
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World2!');
+		if (!request.url) {
+			return new Response('Hello World!');
+		}
+
+
+		const srcURL = new URL(request.url);
+		let newurl:string
+
+		if (request.url.includes('https://')) {
+			newurl = request.url.replace(srcURL.origin + '/', '');
+		} else {
+			newurl = request.url.replace(srcURL.origin, 'https://github.com');
+		}
+
+		const req = new Request(newurl, request);
+		const res = await fetch(req);
+		let newres = new Response(res.body, res);
+
+		let location = newres.headers.get('location');
+		console.log(location);
+		if (location !== null && location !== "") {
+
+			if (request.url.includes('https://')) {
+				location = srcURL.origin+'/'+ location;
+			} else {
+				location = location.replace('https://github.com', srcURL.origin);
+			}
+
+			newres.headers.set('location', location);
+		}
+		return newres
 	},
 } satisfies ExportedHandler<Env>;
